cfcad973b9d9ec3ab1e350bae631009f986a931b
Nityam-Tiwari <nityamtiwari9@gmail.com>
2025-03-14 21:34:14 +0530

    Add temp.java file

---
+++ temp.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class temp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.err.print("enter the temp in celsius:");
+        float temp = sc.nextFloat();
+        System.out.println("temp in fahrenheit:" + ((temp * 9/5) + (32)));
+
+
+
+    }
+    
+}
